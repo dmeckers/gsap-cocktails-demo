443198c58ca33e1a1cab3b78480a415a2f0ed321
dmeckers <morph232@inbox.lv>
2025-07-13 15:39:44 +0300

    Add Vite configuration and metadata files for project setup

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  base: '/gsap-cocktails-demo/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production'
+    ? 'https://dmeckers.github.io/gsap-cocktails-demo/' //TODO::use env
+    : '/',
   plugins: [react(), tailwindcss()],
-})
+}))
